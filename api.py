4cf476fd05b7c6dc7fb818548bee146d26b94c2f
nao501 <79750877+nao501@users.noreply.github.com>
2021-05-10 23:39:18 +0900

    Add files via upload

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ def main():
     keyword = "鬼滅"
     url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword={}&applicationId=1019079537947262807".format(
         keyword)
-    request_deta=get_api(url)["items": [{"item": {"itemName": "a"}}]].values()
+    request_deta=get_api(url)["items": [{"item": {"itemName": "a"}}]]
     print(request_deta)
 
 
