1816d8010d0c2b226ed9eee011339465c00fb191
nao501 <79750877+nao501@users.noreply.github.com>
2021-05-16 20:11:39 +0900

    Add files via upload

--- api.py
+++ api.py
@@ -3,7 +3,6 @@ import urllib
 
 from requests.api import get
 
-GET https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706
 
 def get_api(url):
     result = requests.get(url)
@@ -14,7 +13,7 @@ def main():
     keyword = "鬼滅"
     url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?format=json&keyword={}&applicationId=1019079537947262807".format(
         keyword)
-    request_deta=get_api(url)["items": [{"item": {"itemName": "a"}}]]
+    request_deta=get_api(url).items[0].item.itemName
     print(request_deta)
 
 
